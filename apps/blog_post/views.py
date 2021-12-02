4858b387a1fc929ba8cb21dd713dcd993114de38
fshoukat111 <fshoukat111@gmail.com>
2021-12-02 22:24:11 +0500

    'somesettings'

--- apps/blog_post/views.py
+++ apps/blog_post/views.py
@@ -19,15 +19,26 @@ class CategoriesList(APIView):
         return Response(serializer.data)
 
 # Get PostsList based on Categories
+# class PostsList(APIView):
+
+#     def get(self, request, category_slug):
+#         category_list = get_object_or_404(Categories, slug=category_slug)
+#         posts_list = Posts.objects.filter(category=category_list)
+#         paginator = PageNumberPagination()
+#         pages_of_posts = paginator.paginate_queryset(
+#             posts_list, request, view=self)
+#         serializer = PostsSerializer(pages_of_posts, many=True)
+#         return Response(serializer.data)
+
 class PostsList(APIView):
 
     def get(self, request, category_slug):
         category_list = get_object_or_404(Categories, slug=category_slug)
         posts_list = Posts.objects.filter(category=category_list)
-        paginator = PageNumberPagination()
-        pages_of_posts = paginator.paginate_queryset(
-            posts_list, request, view=self)
-        serializer = PostsSerializer(pages_of_posts, many=True)
+        # paginator = PageNumberPagination()
+        # pages_of_posts = paginator.paginate_queryset(
+        #     posts_list, request, view=self)
+        serializer = PostsSerializer(posts_list, many=True)
         return Response(serializer.data)
 
 #Get Single Post Detail
