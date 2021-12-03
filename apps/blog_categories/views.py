f542dcb6cb5efcfb64c4899c2ba01b634cb986b5
fshoukat111 <fshoukat111@gmail.com>
2021-12-03 23:19:06 +0500

    'minor'

--- apps/blog_categories/views.py
+++ apps/blog_categories/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from rest_framework import response
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from apps.blog_categories.models import *
 from apps.blog_categories.serializers import *
-# Create your views here.
 
+# Create your views here.
 # Get CategoriesList
 class CategoriesList(APIView):
-
+        
     def get(self, request):
         category_list = Categories.objects.all()
         serializer = CategoriesSerializer(category_list, many=True)
-        return response(serializer.data)
+        return Response(serializer.data)
